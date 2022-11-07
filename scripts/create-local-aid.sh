840d5e702045f29290a05b4c70bc8342925fb02e
Kevin Griffin <griffin.kev@gmail.com>
2022-11-07 16:36:35 -0500

    clean up for running without docker and env fixes. Closes #5 #4 #3 #2

--- scripts/create-local-aid.sh
+++ scripts/create-local-aid.sh
@@ -23,10 +23,10 @@ if [ $ret -eq 0 ]; then
 fi
 
 # Create the local database environment (directories, datastore, keystore)
-kli init --name "${QAR_NAME}" --salt "${salt}" --passcode "${passcode}" --config-dir /scripts --config-file qar-config.json
+kli init --name "${QAR_NAME}" --salt "${salt}" --passcode "${passcode}" --config-dir ./scripts --config-file qar-config.json
 
 # Create your local AID for use as a participant in the External AID
-kli incept --name "${QAR_NAME}" --alias "${QAR_ALIAS}" --passcode "${passcode}" --file /scripts/qar-local-incept.json
+kli incept --name "${QAR_NAME}" --alias "${QAR_ALIAS}" --passcode "${passcode}" --file ./scripts/qar-local-incept.json
 
 # Here's your AID:
 kli status --name "${QAR_NAME}" --alias "${QAR_ALIAS}" --passcode "${passcode}"
