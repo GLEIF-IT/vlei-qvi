c29619b9321bb93628ed14d66f7f1725cfe3023d
Kevin Griffin <griffin.kev@gmail.com>
2022-11-09 12:40:30 -0500

    wip

---
+++ scripts/issue-legal-entity-credential.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##################################################################
+##                                                              ##
+##             Script for creating multisig aid                 ##
+##                                                              ##
+##################################################################
+
+PWD=$(pwd)
+source $PWD/source.sh
+
+# Capture password and salt
+passcode="$(get_passcode $1)"
+
+read -p "Enter the LEI of the new Legal Entity: " -r lei
+read -p "Enter the alias of the new Legal Entity: " -r recipient
+
+echo \"${lei}\" | jq -f "${QAR_SCRIPT_DIR}/legal-entity-data.jq" > "${QAR_DATA_DIR}/legal-entity-data.json"
+
+kli vc issue --name "${QAR_NAME}" --passcode "${passcode}" --alias "${QAR_ALIAS}" --registry-name "${QAR_REG_NAME}" --schema EK0jwjJbtYLIynGtmXXLO5MGJ7BDuX2vr2_MhM9QjAxZ --recipient "${recipient}" --data @"${QAR_DATA_DIR}/legal-entity-data.json"
