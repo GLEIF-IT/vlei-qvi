0149061c89e092d0f59609bdd8e17d00dea75b02
Daniel Hardman <daniel.hardman@gmail.com>
2022-12-20 16:29:29 -0700

    fix /data -> data in auth cred scripts
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- scripts/issue-ecr-auth-credential.sh
+++ scripts/issue-ecr-auth-credential.sh
@@ -26,7 +26,7 @@ read -p "Enter AID of QVI : " -r recipient
 le_said=$(kli vc list --name "${QAR_NAME}" --passcode "${passcode}" --alias "${QAR_AID_ALIAS}" --said --schema ENPXp1vQzRF6JwIuS-mp2U8Uf1MoADoP_GqQ62VsDZWY  | tr -d '\r')
 
 echo "\"${le_said}\"" | jq -f "${QAR_SCRIPT_DIR}/ecr-auth-edges-filter.jq" > "${QAR_DATA_DIR}/ecr-auth-edge-data.json"
-kli saidify --file /data/ecr-auth-edge-data.json
+kli saidify --file data/ecr-auth-edge-data.json
 
 # wip
-kli vc issue --name "${QAR_NAME}" --passcode "${passcode}" --alias "${QAR_AID_ALIAS}" --registry-name "${QAR_REG_NAME}" --schema ED_PcIn1wFDe0GB0W7Bk9I4Q_c9bQJZCM2w7Ex9Plsta --recipient "${recipient}" --data @"/data/ecr-auth-data.json" --edges @"/data/ecr-auth-edge-data.json" --rules @"/data/rules.json" --out "/data/credential.json"
+kli vc issue --name "${QAR_NAME}" --passcode "${passcode}" --alias "${QAR_AID_ALIAS}" --registry-name "${QAR_REG_NAME}" --schema ED_PcIn1wFDe0GB0W7Bk9I4Q_c9bQJZCM2w7Ex9Plsta --recipient "${recipient}" --data @"data/ecr-auth-data.json" --edges @"data/ecr-auth-edge-data.json" --rules @"data/rules.json" --out "data/credential.json"
--- scripts/issue-oor-auth-credential.sh
+++ scripts/issue-oor-auth-credential.sh
@@ -26,9 +26,9 @@ echo "[\"${AID}\", \"${lei}\", \"${personLegalName}\", \"${officialRole}\"]" | j
 le_said=$(kli vc list --name "${QAR_NAME}" --passcode "${passcode}" --alias "${QAR_AID_ALIAS}" --said --schema ENPXp1vQzRF6JwIuS-mp2U8Uf1MoADoP_GqQ62VsDZWY  | tr -d '\r')
 
 echo "\"${le_said}\"" | jq -f "${QAR_SCRIPT_DIR}/oor-auth-edges-filter.jq" > "${QAR_DATA_DIR}/oor-auth-edge-data.json"
-kli saidify --file /data/oor-auth-edge-data.json
+kli saidify --file data/oor-auth-edge-data.json
 
 # Prepare the RULES section
 cp "${QAR_SCRIPT_DIR}/rules.json" "${QAR_DATA_DIR}/rules.json"
 
-kli vc issue --name "${QAR_NAME}" --passcode "${passcode}" --alias "${QAR_AID_ALIAS}" --registry-name "${QAR_REG_NAME}" --schema EKA57bKBKxr_kN7iN5i7lMUxpMG-s19dRcmov1iDxz-E --recipient "${recipient}" --data @"/data/oor-auth-data.json" --edges @"/data/oor-auth-edge-data.json" --rules @"/data/rules.json" --out "/data/credential.json"
+kli vc issue --name "${QAR_NAME}" --passcode "${passcode}" --alias "${QAR_AID_ALIAS}" --registry-name "${QAR_REG_NAME}" --schema EKA57bKBKxr_kN7iN5i7lMUxpMG-s19dRcmov1iDxz-E --recipient "${recipient}" --data @"data/oor-auth-data.json" --edges @"data/oor-auth-edge-data.json" --rules @"data/rules.json" --out "data/credential.json"
