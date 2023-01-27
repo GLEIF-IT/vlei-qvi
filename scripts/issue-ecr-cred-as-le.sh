4fab61b8ee4c133c56ff62146b75b8a9df14f4fd
Daniel Hardman <daniel.hardman@gmail.com>
2023-01-27 19:03:41 +0100

    fix schema said error in ecr cred script
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- scripts/issue-ecr-cred-as-le.sh
+++ scripts/issue-ecr-cred-as-le.sh
@@ -29,4 +29,4 @@ kli saidify --file data/legal-entity-ecr-edge-data.json
 # Prepare the RULES section
 cp "${QAR_SCRIPT_DIR}/rules.json" "${QAR_DATA_DIR}/rules.json"
 
-kli vc issue --name "${QAR_NAME}" --passcode "${passcode}" --alias "${QAR_AID_ALIAS}" --registry-name "${QAR_REG_NAME}" --schema EBNaNu-M9P5cgrnfl2Fvymy4E_jvxxyjb70PRtiANlJy --recipient "${recipient}" --data @"data/legal-entity-ecr-data.json" --edges @"data/legal-entity-ecr-edge-data.json" --rules @"data/rules.json" --out "data/ecr.json"
+kli vc issue --name "${QAR_NAME}" --passcode "${passcode}" --alias "${QAR_AID_ALIAS}" --registry-name "${QAR_REG_NAME}" --schema EEy9PkikFcANV1l7EHukCeXqrzT1hNZjGlUk7wuMO5jw --recipient "${recipient}" --data @"data/legal-entity-ecr-data.json" --edges @"data/legal-entity-ecr-edge-data.json" --rules @"data/rules.json" --out "data/ecr.json"
