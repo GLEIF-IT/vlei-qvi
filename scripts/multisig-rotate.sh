a6b54b61d342a823d15d1094ae39a664f844217b
pfeairheller <pfeairheller@gmail.com>
2023-07-18 14:33:13 -0700

    QVI Multisig Rotation - fixed script

--- scripts/multisig-rotate.sh
+++ scripts/multisig-rotate.sh
@@ -2,7 +2,7 @@
 
 ##################################################################
 ##                                                              ##
-##             Script for creating multisig aid                 ##
+##             Script for rotating multisig aid                 ##
 ##                                                              ##
 ##################################################################
 
@@ -12,7 +12,4 @@ source $PWD/source.sh
 # Capture password
 passcode=$(get_passcode $1)
 
-read -p "Enter the Alias for the Group Multisig AID: " -r alias
-read -p "Enter the filename of the inception configuration file: " -r filename
-
-kli multisig rotate  --name "${QAR_NAME}" --passcode "${passcode}"  --alias "${QAR_ALIAS}" "$@"
+kli multisig rotate  --name "${QAR_NAME}" --passcode "${passcode}"  --alias "${QAR_AID_ALIAS}" "$@"
